86cb9dc320a56013ef3bb9a088d680bcdcd89a60
sengiyumva123 <158841581+sengiyumva123@users.noreply.github.com>
2025-04-12 12:47:20 -0400

    Add files via upload

---
+++ HealthAndWellness/HealthAndWellness/HealthAndWellnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthAndWellnessApp.swift
+//  HealthAndWellness
+//
+//  Created by Sengi Mathias on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthAndWellnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthAndWellness/HealthAndWellnessTests/HealthAndWellnessTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthAndWellnessTests.swift
+//  HealthAndWellnessTests
+//
+//  Created by Sengi Mathias on 4/12/25.
+//
+
+import Testing
+@testable import HealthAndWellness
+
+struct HealthAndWellnessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
